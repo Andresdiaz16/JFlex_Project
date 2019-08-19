46a9ec80400592b9e3740a49386f87c7491edbec
Andresdiaz16 <45214848+Andresdiaz16@users.noreply.github.com>
2019-08-18 21:20:18 -0600

    Add files via upload

---
+++ Lexer/src/lexer/Lexer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexer;
+
+/**
+ *
+ * @author Andres Diaz
+ */
+public class Lexer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static view newView = new view();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        newView.setVisible(true);
+        newView.setResizable(false);
+        newView.setLocationRelativeTo(null);
+    }
+    
+}
